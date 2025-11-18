15c96b2ae828ba805c61fa1b3092b0c328030bcf
Eros Taborelli <eros.taborelli@erost.net>
2025-11-18 16:01:24 +0100

    fix(pwa): Remove wrong console statement

--- src/main.js
+++ src/main.js
@@ -18,11 +18,8 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('/sw.js')
-      .then((registration) => {
-        console.log('Service Worker registered:', registration.scope);
-      })
       .catch((error) => {
-        console.log('Service Worker registration failed:', error);
+        console.error('Service Worker registration failed:', error);
       });
   });
 }
